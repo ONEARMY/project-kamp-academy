ef34ada7a85122499ee8438a68840cffbfc191a4
Dave Hakkens <dave@davehakkens.nl>
2020-08-21 19:50:11 +0200

    change projectname

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -33,7 +33,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'project-kamp-academy',
+  projectName: 'academy',
   organizationName: 'davehakkens',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
